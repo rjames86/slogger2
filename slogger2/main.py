f21e5045e8986dec1afbaae918640ce8fb43b465
Ryan M <ryan@ryanmo.co>
2016-04-03 10:07:26 -0700

    updating requirements

--- slogger2/main.py
+++ slogger2/main.py
@@ -1,6 +1,7 @@
 from registry import Plugins
 from writers import PlistWriter, Writers
 
+
 def main():
     plugins = Plugins.get()
     dayone_entries = []
@@ -10,13 +11,10 @@ def main():
             to_write = Writers.from_entries(map(PlistWriter, dayone_entries))
             # to_write.write("/Users/rjames/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Auto Import/Default Journal.dayone")
             to_write.write("/Users/rjames/Dropbox/Apps/Day One/Journal.dayone")
-        except Exception as e:
-            import pdb; pdb.set_trace()
+        except Exception:
             continue
         else:
             plugin.on_success()
-
-
 
 if __name__ == '__main__':
     main()
